7ad77ad25cb7e8b752f0c827f547e84a6755ab35
Adam <adamssmith2003@gmail.com>
2023-04-21 19:30:07 +0400

    last

--- test.py
+++ test.py
@@ -60,5 +60,171 @@ import numpy as np
 # plt.plot(x, y)
 # plt.show()
 
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.plot(x, y)
+
+# plt.grid(axis="y", linestyle = '--', linewidth = 0.5) #axis = "x"
+
+# plt.show()
 
 
+#plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(1, 2, 1)
+# #the figure has 1 row, 2 columns, and this plot is the first plot
+# plt.plot(x,y)
+
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(1, 2, 2)
+# #the figure has 1 row, 2 columns, and this plot is the second plot
+# plt.plot(x,y)
+
+# plt.show()
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(2, 3, 1)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(2, 3, 2)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(2, 3, 3)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(2, 3, 4)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(2, 3, 5)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(2, 3, 6)
+# plt.plot(x,y)
+
+# plt.show()
+
+#plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
+# plt.title("SALES")
+
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+# plt.title("INCOME")
+
+# plt.show()
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+
+# plt.scatter(x, y)
+# plt.show()
+
+
+#day one, the age and speed of 13 cars:
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# plt.scatter(x, y)
+
+# #day two, the age and speed of 15 cars:
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x, y)
+
+# plt.show()
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# # colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+# colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100]) #colormap
+# plt.scatter(x, y, c=colors, cmap='viridis')
+# plt.colorbar()
+# plt.show()
+
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+
+# plt.scatter(x, y, s=sizes, alpha=0.5) #alpha is opacity
+
+# plt.show()
+
+# x = np.random.randint(100, size=(100))
+# y = np.random.randint(100, size=(100))
+# colors = np.random.randint(100, size=(100))
+# sizes = 10 * np.random.randint(100, size=(100))
+
+# plt.scatter(x, y, c=colors, s=sizes, alpha=0.5, cmap='nipy_spectral')
+
+# plt.colorbar()
+
+# plt.show()
+
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+
+# plt.bar(x,y)
+# plt.show()
+
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+
+# plt.bar(x, y, color="red", width=0.1) #barh horizontal
+# plt.show()
+
+# x = np.random.normal(170, 10, 250) #standart deviation Normal DATA distribution
+
+# plt.hist(x)
+# plt.show() 
+
+# y = np.array([35, 25, 25, 15])
+# mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+# plt.pie(y, labels = mylabels, startangle = 90)
+# plt.show() 
+
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+
+plt.pie(y, labels = mylabels, colors = mycolors)
+myexplode = [0.2, 0, 0, 0]
+
+# plt.pie(y, labels = mylabels, explode = myexplode, shadow=True)
+plt.legend(title = "Four Fruits:")
+plt.show() 
