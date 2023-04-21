e5c2453ab50b4dec9576c5d3c3d33d05bf9b49e5
Adam <adamssmith2003@gmail.com>
2023-04-21 16:15:23 +0400

    l2

--- test.py
+++ test.py
@@ -13,7 +13,52 @@ import numpy as np
 # plt.plot(xpoints, ypoints)
 # plt.show()
 
-ypoints = np.array([3, 8, 1, 10, 5, 7]) # x-axis dafault [0, 1, 2, 3, 4 .....]
+# ypoints = np.array([3, 8, 1, 10, 5, 7]) # x-axis dafault [0, 1, 2, 3, 4 .....]
 
-plt.plot(ypoints)
-plt.show()
+# plt.plot(ypoints, marker = 'o', ms = 10, mec = "r", mfc = 'r')
+# plt.plot(ypoints, linestyle = 'dashed', color = "red", linewidth = "15")
+
+# y1 = np.array([3, 8, 1, 10]) #two different lines
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(y1)
+# plt.plot(y2)
+
+# plt.show()
+
+# x1 = np.array([0, 1, 2, 3])
+# y1 = np.array([3, 8, 1, 10])
+# x2 = np.array([0, 1, 2, 3])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(x1, y1, x2, y2)
+# plt.show()
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125]) #labels
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+# plt.title("table title")
+# plt.xlabel("x label")
+# plt.ylabel("y label")
+
+# plt.show()
+
+
+
+#Font for table
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# font1 = {'family':'serif','color':'blue','size':20} 
+# font2 = {'family':'serif','color':'darkred','size':15}
+
+# plt.title("Sports Watch Data", fontdict = font1)
+# plt.xlabel("Average Pulse", fontdict = font2, loc="left") #loc for position
+# plt.ylabel("Calorie Burnage", fontdict = font2)
+
+# plt.plot(x, y)
+# plt.show()
+
+
+
